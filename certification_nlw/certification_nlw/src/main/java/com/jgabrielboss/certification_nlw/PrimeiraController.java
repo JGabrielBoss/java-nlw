fc537f65319f8b34d44ad345d02c4b69c65ab412
JGabrielBoss <112196857+JGabrielBoss@users.noreply.github.com>
2024-02-11 23:18:20 -0300

    Primeiro Post
    Retornando um @Post

--- certification_nlw/certification_nlw/src/main/java/com/jgabrielboss/certification_nlw/PrimeiraController.java
+++ certification_nlw/certification_nlw/src/main/java/com/jgabrielboss/certification_nlw/PrimeiraController.java
@@ -3,6 +3,8 @@ package com.jgabrielboss.certification_nlw;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 @RestController
@@ -13,5 +15,10 @@ public class PrimeiraController {
     @GetMapping("/retornarPrimeiraController")
     public String retornoController() {
         return "Minha primeira Controller!!";
+    }
+
+    @PostMapping("/retornaPost")
+    public String primeiroPost(){
+    return "Meu primerio POST";
     }
 }
