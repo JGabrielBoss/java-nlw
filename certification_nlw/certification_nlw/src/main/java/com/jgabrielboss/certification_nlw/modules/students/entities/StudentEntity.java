cfffece6ba4e3ea68f964224c26aafcd7c7a9791
JGabrielBoss <112196857+JGabrielBoss@users.noreply.github.com>
2024-02-14 00:59:03 -0300

    Atualizando o projeto
    Criança tabelas de relacionamento para o db

--- certification_nlw/certification_nlw/src/main/java/com/jgabrielboss/certification_nlw/modules/students/entities/StudentEntity.java
+++ certification_nlw/certification_nlw/src/main/java/com/jgabrielboss/certification_nlw/modules/students/entities/StudentEntity.java
@@ -3,6 +3,13 @@ package com.jgabrielboss.certification_nlw.modules.students.entities;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +17,17 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "students")
 public class StudentEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @OneToMany(mappedBy = "StudentEntity")
     private List<CertificationStudentEntity> certificationStudentEntity;
     
 }
